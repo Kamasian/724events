0398ed9e1af3bb62a59543603ce376aeb811a45c
Dugrais Emmanuel <emmanuel.dugrais@gmail.com>
2023-07-30 13:52:02 +0200

    Ajout de la dernière prestation en bas de page (création d'une variable last + conditions)

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -12,7 +12,9 @@ import Modal from "../../containers/Modal";
 import { useData } from "../../contexts/DataContext";
 
 const Page = () => {
-    const { last } = useData()
+    const { data } = useData()
+    const last = data?.events[data.events.length - 1];
+
     return <>
         <header>
             <Menu />
@@ -115,13 +117,17 @@ const Page = () => {
         <footer className="row">
             <div className="col presta">
                 <h3>Notre derniére prestation</h3>
-                <EventCard
-                    imageSrc={last?.cover}
-                    title={last?.title}
-                    date={new Date(last?.date)}
-                    small
-                    label="boom"
-                />
+                {last && last.cover && last.title ? (
+                    <EventCard
+                        imageSrc={last.cover}
+                        title={last.title}
+                        date={new Date(last.date)}
+                        small
+                        label="boom"
+                    />
+                ) : (
+                    null
+                )};
             </div>
             <div className="col contact">
                 <h3>Contactez-nous</h3>
